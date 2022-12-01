4a4cdad62142109668f054bd544ae0969222036d
treut <d@treut.ru>
2022-12-01 22:57:12 +0300

    fix

--- task_01.py
+++ task_01.py
@@ -3,9 +3,12 @@ import re
 
 def is_palindrome(string):
     string = re.sub(r'[ \-,.\'\"!]', "", str(string).lower())
-    rev = list(string)
-    rev.reverse()
-    return "".join(rev) == string
+    return string == string[::-1]
+
+    #v1.0
+    # rev = list(string)
+    # rev.reverse()
+    # return "".join(rev) == string
 
 
 examples = ["A man, a plan, a canal -- Panama", "Madam, I'm Adam!", 333, None, "Abracadabra"]
