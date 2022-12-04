2c3eae7869000ea8f7f39427407b7ab421df9eed
treut <d@treut.ru>
2022-12-04 15:24:44 +0300

    task_07

---
+++ task_07.py
@@ -0,0 +1,14 @@
+def combine_anagrams(words_array):
+    result = {}
+    for k, v in enumerate(words_array):
+        conv = "".join(sorted(list(str(words_array[k]).lower())))
+
+        if not conv in result.keys():
+            result[conv] = []
+        result[conv].append(words_array[k])
+    return list(result.values())
+
+
+example = ["cars", "for", "potatoes", "racs", "four", "scar", "creams", "scream"]
+
+print(combine_anagrams(example))
