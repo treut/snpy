4380f2ddd2dd02c97a97c53cb43218e114a8a837
treut <d@treut.ru>
2022-12-05 00:24:32 +0300

    task_10

---
+++ task_10.py
@@ -0,0 +1,18 @@
+import re
+
+
+def count_words(string_ex):
+    result = {}
+    string_ex = re.split(r'([a-zа-я]+)', str(string_ex).lower())
+    for k, v in enumerate(string_ex):
+        if re.sub(r'[^a-zа-я]', "", v) == "":
+            continue
+        if v not in result.keys():
+            result[v] = 0
+        result[v] += 1
+    return result
+
+
+print(count_words("A man, a plan, a canal -- Panama"))
+print(count_words("Doo bee doo bee doo"))
+print(count_words("A man, a plan, a canal -- Panama тест текст тест тест"))
